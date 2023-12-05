cf3fe39d86d08792acc2a5038cb053d54eb8824b
brand-steps <bhaia9036@gmail.com>
2023-12-05 17:28:18 +0500

    first commit

--- src/Components/Header/NavContact2.jsx
+++ src/Components/Header/NavContact2.jsx
@@ -43,23 +43,23 @@ backgroundColor: '#00b0db'    /* Other styles you might want to apply */
 
   return (
     <Navbar fluid rounded className=' border-b-2 border-rose-500 text-black bg-slate-200'>
-    <Navbar.Brand href="https://flowbite-react.com">
+    <Navbar.Brand href="">
     <img src={phone} className="mr-3 inline h-7 " alt="Flowbite React Logo" />
-      <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">01273 569355</span>
+      <span className="self-center whitespace-nowrap text-xl font-semibold">01273 569355</span>
     </Navbar.Brand>
     <div className="flex md:order-2">
       <p className='text-black'>      
         <img src={insta} className="mr-3 inline h-7 " alt="Flowbite React Logo" />
         <img src={face} className="mr-3 inline h-8 " alt="Flowbite React Logo" />
 </p>
-      <Navbar.Toggle />
+      
     </div>
     <Navbar.Collapse>
 
-    <Navbar.Brand href="https://flowbite-react.com">
+    <Navbar.Brand href="">
         
     <img src={goog} className="mr-3 inline h-7 " alt="Flowbite React Logo" />
-     <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Unit 19 Beach Cl, Newhaven BN9 0BY</span>
+     <span className="self-center whitespace-nowrap text-xl font-semibold">Unit 19 Beach Cl, Newhaven BN9 0BY</span>
     </Navbar.Brand>
 
     </Navbar.Collapse>
